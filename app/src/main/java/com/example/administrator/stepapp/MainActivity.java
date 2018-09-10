0bd37ad8cfd0e4b52b1f0370773f5478d161ebf9
Sun <sun@gmail.com>
2018-09-10 08:36:12 +0800

    修改bug

--- app/src/main/java/com/example/administrator/stepapp/MainActivity.java
+++ app/src/main/java/com/example/administrator/stepapp/MainActivity.java
@@ -29,6 +29,7 @@ import com.example.administrator.frame.BaseActivity;
 import com.example.administrator.model.PedometerChartBean;
 import com.example.administrator.service.IPedometerService;
 import com.example.administrator.service.PedometerService;
+import com.example.administrator.util.DateUtils;
 import com.example.administrator.util.LogWriter;
 import com.example.administrator.util.Settings;
 import com.example.administrator.util.Utils;
@@ -51,10 +52,23 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.OnClick;
+import lecho.lib.hellocharts.animation.ChartAnimationListener;
+import lecho.lib.hellocharts.formatter.SimpleLineChartValueFormatter;
+import lecho.lib.hellocharts.gesture.ContainerScrollType;
+import lecho.lib.hellocharts.gesture.ZoomType;
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.ValueShape;
+import lecho.lib.hellocharts.model.Viewport;
+import lecho.lib.hellocharts.view.LineChartView;
 
 import static com.example.administrator.util.DateUtils.*;
 
 public class MainActivity extends AppBaseActivity {
+    private static final String TAG = "MainActivity";
     @BindView(R.id.btnStart)
     protected Button startButton;
     @BindView(R.id.reset)
@@ -73,9 +87,13 @@ public class MainActivity extends AppBaseActivity {
     protected TextView distance;
     @BindView(R.id.tv_target_distance)
     protected TextView tvTargetDistance;
-    @BindView(R.id.chart1)
+   /* @BindView(R.id.chart1)
+    protected BarChart mChart;*/
 
-    protected BarChart mChart;
+    @BindView(R.id.step_chart)
+    protected LineChartView lineChart;
+    private static final long MINUTE_MILLISECONDS = 1000 * 60;
+    private long startTime;
     private static final int STATUS_NOT_RUNNING = 0;
     private static final int STATUS_RUNNING = 1;
     private static final long SLEEP_TIME = 60000L;
@@ -92,18 +110,21 @@ public class MainActivity extends AppBaseActivity {
 
     private Settings setting = null;
 
-
+    private List<PointValue> mPointValues;
+    private List<PointValue> mStartPointValues;
+    private List<AxisValue> mAxisXValues = new ArrayList<AxisValue>();
+    //String[] date = {"01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "24:00"};//X轴的标注
     Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             switch (msg.what) {
                 case MESSAGE_UPDATE_STEP_COUNT: {
-                  //  handler.removeMessages(MESSAGE_UPDATE_STEP_COUNT);
+                    //  handler.removeMessages(MESSAGE_UPDATE_STEP_COUNT);
                     updateStepCount();
                 }
                 break;
                 case MESSAGE_UPDATE_CHART: {
-                  //  handler.removeMessages(MESSAGE_UPDATE_CHART);
+                    //  handler.removeMessages(MESSAGE_UPDATE_CHART);
                     if (chartBean != null) {
                         setData(chartBean);
                     }
@@ -115,12 +136,50 @@ public class MainActivity extends AppBaseActivity {
         }
     };
 
+    private float x;
 
     public void setData(PedometerChartBean pedometerChartBean) {
-        ArrayList<BarEntry> yVals = new ArrayList<>();
         if (pedometerChartBean != null) {
             for (int i = 0; i < pedometerChartBean.getIndex(); i++) {
                 int valY = pedometerChartBean.getDataArray()[i];
+                Log.e(TAG, "setData: valY = " + valY);
+                mPointValues.add(new PointValue(i, valY));
+                x = mPointValues.get(i).getX();
+            }
+            //根据新的点集合画出新的线
+            ////////////////////////////////////////
+            Line line = new Line(mPointValues).setColor(Color.RED);
+            line.setShape(ValueShape.CIRCLE);//折线图上每个数据点的形状  这里是圆形 （有三种 ：ValueShape.SQUARE  ValueShape.CIRCLE  ValueShape.DIAMOND）
+            line.setCubic(false);//曲线是否平滑，即是曲线还是折线
+            line.setFilled(false);//是否填充曲线的面积
+            //  line.setFormatter(new SimpleLineChartValueFormatter(1));
+            line.setHasLabels(true);//曲线的数据坐标是否加上备注
+
+            line.setPointRadius(3);//坐标点的大小
+            line.setHasLabelsOnlyForSelected(true);//点击数据坐标提示数据（设置了这个line.setHasLabels(true);就无效）
+
+            line.setHasLines(true);//是否用线显示。如果为false 则没有曲线只有点显示
+            line.setHasPoints(true);//是否显示圆点 如果为false 则没有原点只有点显示（每个数据点都是个大的圆点）
+            ///////////////////////////////////////////////////////////////////
+            lines.add(line);
+            mLineChartData.setLines(lines);
+            lineChart.setLineChartData(mLineChartData);
+            Viewport port;
+            if(x > 5){
+                port = initViewPort(x-5,x);
+            }
+            else {
+                port = initViewPort(0,5);
+            }
+            lineChart.setMaximumViewport(port);
+            lineChart.setCurrentViewport(port);
+        }
+        Log.e(TAG, "getAxisPoints");
+      /*  ArrayList<BarEntry> yVals = new ArrayList<>();
+        if (pedometerChartBean != null) {
+            for (int i = 0; i < pedometerChartBean.getIndex(); i++) {
+                int valY = pedometerChartBean.getDataArray()[i];
+                ///////////////////////////////////////////////////////////图表点
                 yVals.add(new BarEntry(i, valY));
             }
             time.setText(String.valueOf(pedometerChartBean.getIndex()) + "分");
@@ -132,8 +191,30 @@ public class MainActivity extends AppBaseActivity {
             mChart.setData(data);
             mChart.notifyDataSetChanged();
             mChart.invalidate();
-        }
+        }*/
     }
+
+    /**
+     * 设置X 轴的显示
+     */
+ /*   private void getAxisXLables() {
+        for (int i = 0; i < date.length; i++) {
+            mAxisXValues.add(new AxisValue(i).setLabel(date[i]));
+        }
+        Log.e(TAG, "getAxisXLables");
+    }*/
+
+    /**
+     * 图表的每个点的显示
+     */
+  /*  private void getAxisPoints() {
+
+        for (int i = 0; i <  dataList.size(); i++) {
+            mPointValues.add(new PointValue(i, Float.parseFloat(dataList.get(i))));
+            // mPointValues.setLabel(date.getValue()+"℃");
+        }
+        Log.e(TAG,"getAxisPoints");
+    }*/
 
     private volatile boolean isChartUpdate = false;
 
@@ -193,6 +274,10 @@ public class MainActivity extends AppBaseActivity {
             calorieCount.setText(Utils.getFormatVal(calorieVal, "0.00") + "卡");
             distance.setText(Utils.getFormatVal(distanceVal, "0.00"));
             circleProgressBar.setProgress(stepCountVal);
+            long nowTime = System.currentTimeMillis();
+            long elapseTime = nowTime - startTime;
+            time.setText(String.valueOf(elapseTime / MINUTE_MILLISECONDS));
+
         }
     }
 
@@ -297,10 +382,13 @@ public class MainActivity extends AppBaseActivity {
         super.initData(savedInstanceState);
         initStepOptionPicker();
         setting = new Settings(MainActivity.this);
-        circleProgressBar.setMax( setting.getTargetDiatance());
+        circleProgressBar.setMax(setting.getTargetDiatance());
         Log.e("888", "onInitView: circleProgressBar.getMax() " + circleProgressBar.getMax());
         circleProgressBar.setProgress(0);
-        setBarChatStyle();
+        /////////////////////////////////////////
+        //setBarChatStyle();
+    //    getAxisXLables();
+        initLineChart();
         time.setText("0");
         stepCount.setText("0步");
         calorieCount.setText("0.00卡");
@@ -367,6 +455,8 @@ public class MainActivity extends AppBaseActivity {
 
     @OnClick(R.id.btnStart)
     public void startClick() {
+        //点击开始按钮的时间
+        startTime = System.currentTimeMillis();
         try {
             status = mRemoteService.getServiceRunningStatus();
         } catch (RemoteException e) {
@@ -410,7 +500,73 @@ public class MainActivity extends AppBaseActivity {
         }
     }
 
-    private void setBarChatStyle() {
+    private List<Line> lines;
+    private LineChartData mLineChartData;
+
+    private void initLineChart() {
+        mPointValues = new ArrayList<PointValue>();
+     //   mPointValues.add(new PointValue(0, 0));
+        lines = new ArrayList<Line>();
+        mLineChartData = new LineChartData();
+        mLineChartData.setLines(null);//////////////////////////////////////////////
+
+        //设置图表上点的背景色，如果不设置，图表上点的背景色就与线的颜色一样
+        mLineChartData.setValueLabelBackgroundEnabled(true);
+        mLineChartData.setValueLabelBackgroundAuto(false);
+        mLineChartData.setValueLabelBackgroundColor(Color.parseColor("#FF0099CC"));
+
+        //坐标轴
+        Axis axisX = new Axis(); //X轴
+        axisX.setHasTiltedLabels(true);  //X坐标轴字体是斜的显示还是直的，true是斜的显示
+        axisX.setTextColor(Color.parseColor("#FF0099CC"));  //设置字体颜色
+        axisX.setName("分钟");  //表格名称
+        axisX.setTextSize(10);//设置字体大小
+        axisX.setMaxLabelChars(8); //最多几个X轴坐标，意思就是你的缩放让X轴上数据的个数7<=x<=mAxisXValues.length
+        axisX.setValues(mAxisXValues);  //填充X轴的坐标名称
+        mLineChartData.setAxisXBottom(axisX); //x 轴在底部
+        axisX.setHasLines(true); //x 轴分割线
+
+        //  Y轴是根据数据的大小自动设置Y轴上限(在下面我会给出固定Y轴数据个数的解决方案)
+        Axis axisY = new Axis();  //Y轴
+        axisY.setName("步数/步");//y轴标注
+        axisY.setTextSize(10);//设置字体大小
+        axisY.setMaxLabelChars(7);//默认是3，只能看到最后三个数字
+        axisY.setHasLines(true);
+        axisY.setTextColor(Color.parseColor("#FF0099CC"));
+        mLineChartData.setAxisYLeft(axisY);  //Y轴设置在左边
+//////////////////////////////////////////////////////////////////////
+
+        //设置行为属性，支持缩放、滑动以及平移
+        lineChart.setInteractive(true);//设置图表是否可以与用户交互
+        lineChart.setZoomType(ZoomType.HORIZONTAL);
+        lineChart.setMaxZoom((float) 4);//最大方法比例
+        lineChart.setContainerScrollEnabled(true, ContainerScrollType.HORIZONTAL);
+        lineChart.setLineChartData(mLineChartData);//为图表设置数据，数据类型为LineChartData
+        lineChart.setVisibility(View.VISIBLE);
+
+        Viewport viewport = initViewPort(0, 5);
+        lineChart.setMaximumViewport(viewport);
+   /*     float minY = 0f;
+        float maxY = 100;
+        Viewport viewport = new Viewport(lineChart.getMaximumViewport());
+        viewport.bottom = minY;
+        viewport.top = maxY;
+        lineChart.setMaximumViewport(viewport);
+        viewport.left = 0;
+        lineChart.setCurrentViewport(viewport);*/
+      //  lineChart.startDataAnimation();
+    }
+    private Viewport initViewPort(float left,float right) {
+        Viewport port = new Viewport();
+        port.top = 1000;
+        port.bottom = 0;
+        port.left = left;
+        port.right = right;
+        return port;
+    }
+
+
+  /*  private void setBarChatStyle() {
         mChart.setBackgroundColor(Color.WHITE);
         mChart.setDrawBarShadow(false);//取消阴影
         mChart.setDrawValueAboveBar(true);
@@ -429,7 +585,7 @@ public class MainActivity extends AppBaseActivity {
         mChart.setPinchZoom(true);//取消缩放
         mChart.setDoubleTapToZoomEnabled(false);
         mChart.setDrawGridBackground(false);
-    }
+    }*/
 
 
     private void requestData() {
